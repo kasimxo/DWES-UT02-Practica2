1ec23cfddeab2270e9e81eabb5a7f3da859d0e45
kasimxo <kasimxo@gmail.com>
2025-11-05 22:53:23 +0100

    first commit

---
+++ ejercicio1-php/pagos.php
@@ -0,0 +1,9 @@
+<?php
+echo "Abanosl";
+
+// Array asociativo multidimensional de socios
+// Cada socio tiene id, nombre, apellidos, dni, email y teléfono
+
+// Además, cada socio tiene un array asociativo de pagos mensuales del año en curso
+$socios = array(1=>array("andres"));           
+?>
