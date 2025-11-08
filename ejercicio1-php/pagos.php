2c154340c982ec388388ca52f257e63a34a867c7
kasimxo <kasimxo@gmail.com>
2025-11-08 10:26:58 +0100

    Finished

--- ejercicio1-php/pagos.php
+++ ejercicio1-php/pagos.php
@@ -1,11 +1,31 @@
 <?php
 
-// Array asociativo multidimensional de socios
-// Cada socio tiene id, nombre, apellidos, dni, email y teléfono
+/**
+ * Información extraída de: www.php.net/manual/en/language.types.array.php
+ * 
+ * Creación de arrays
+ * Los arrays se pueden crear utilizando el constructor array() o directamente los caracteres []
+ * 
+ * Arrays indexados:
+ * Un array indexado es aquel en el que cada elemento tiene asignado un número.
+ * $array_indexado = [0 => 1, 1 => 2, 2 => 3];
+ * Si no se declara el valor del índice de forma explícita, php lo asigna de forma automática.
+ * $array_indexado = [1, 2, 3]; // es lo mismo que el array anterior
+ * 
+ * Arrays asociativos:
+ * Un array asociativo es aquel en el que cada elemento tiene asignada una cadena.
+ * $array_asociativo = ["primero" => 1, "segundo" => 2, "tercero" => 3];
+ * 
+ * Es importante tener en cuenta que en realidad, php trata todos los arrays como 'mapas' clave-valor.
+ * Esto hace que no haya una diferencia significativa entre un array indexado y uno asociativo,
+ * e incluso resulta posible crear un array mixto.
+ * $array_mixto = ["primero" => 1, 2 => 2, "tercero" => 3];
+ * 
+ * Arrays multidimensionales:
+ * Un array multidimensional es aquel que alberga otros arrays
+ * $array_multidimensional = [[1, 2, 3], [1, 2, 3]];
+ */
 
-// Además, cada socio tiene un array asociativo de pagos mensuales del año en curso
-// AAAA-MM
-// nombre del mes / importe cuota / Pagado/Pendiente / Fecha de pago o null
 $socios = [
     1 => [
         "id" => 1,
@@ -78,12 +98,6 @@ $socios = [
             "2025-11" => [
                 "mes" => "noviembre",
                 "importe" => 123,
-                "estado" => "Pendiente",
-                "fecha_pago" => null
-            ],
-            "2025-12" => [
-                "mes" => "diciembre",
-                "importe" => 830,
                 "estado" => "Pendiente",
                 "fecha_pago" => null
             ]
@@ -162,32 +176,41 @@ $socios = [
                 "importe" => 199,
                 "estado" => "Pendiente",
                 "fecha_pago" => null
-            ],
-            "2025-12" => [
-                "mes" => "diciembre",
-                "importe" => 730,
-                "estado" => "Pendiente",
-                "fecha_pago" => null
             ]
         ]
     ]
 ];           
 
+echo "<h1>Listado de socios</h1>";
 foreach ($socios as $socio){
+    echo "<hr>";
     echo "<div>";
-    echo "{$socio['nombre']}";
+    echo "<p>Nombre: {$socio['nombre']} {$socio['apellidos']}</p>";
+    echo "<p>Dni: {$socio['dni']}</p>";
+    echo "<p>Email: {$socio['email']}</p>";
+    echo "<p>Teléfono: {$socio['telefono']}</p>";
     echo "<table>";
     echo "<tr><th>Mes</th><th>Importe</th><th>Estado</th><th>Fecha de pago</th></tr>";
-    $total = 0;
+    $total_pagado = 0;
+    $total_a_pagar = 0;
     foreach ($socio['pagos'] as $pago){
-        echo "<tr><td>{$pago['mes']}</td><td>{$pago['importe']}</td><td>{$pago['estado']}</td><td>{$pago['fecha_pago']}</td></tr>";
+        $color = '#f7adadff';
         if ($pago['estado'] == "Pagado"){
-            $total += $pago['importe'];
+            $color = 'transparent';
+            $total_pagado += $pago['importe'];
+        } else {
+            $total_a_pagar += $pago['importe'];
         }
+        echo "<tr>";
+        echo "<td>{$pago['mes']}</td>";
+        echo "<td style='background-color:{$color};' >{$pago['importe']}</td>";
+        echo "<td>{$pago['estado']}</td>";
+        echo "<td>{$pago['fecha_pago']}</td>";
+        echo "</tr>";
     }
-    echo "<tr><td>{$total}</td></tr>";
+    echo "<tr><td>Total pagado:</td><td colspan='3'>{$total_pagado}</td></tr>";
+    echo "<tr><td>Total a pagar:</td><td colspan='3' style='background-color:{$color};'>{$total_a_pagar}</td></tr>";
     echo "</table>";
     echo "</div>";
 }
-
 ?>
